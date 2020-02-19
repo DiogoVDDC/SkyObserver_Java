ebd30cadc8c3af5813d67725d7f6e60caa52150f
diogo <diogo@10.100.3.215>
2020-02-19 21:31:43 +0100

    Class angle

--- ProjetAstronomy/src/Main.java
+++ ProjetAstronomy/src/Main.java
@@ -1,8 +1,9 @@
+import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.Polynomial;
 
 public class Main {
     public static void main(String[] args) {
-        Polynomial wow = Polynomial.of(10, 2, 45);
-        System.out.println(wow);
+       System.out.println(Angle.ofArcsec(1));
+       System.out.println(Angle.ofDMS(85, 18, 30));
     }
 }
--- ProjetAstronomy/src/ch/epfl/rigel/math/Angle.java
+++ ProjetAstronomy/src/ch/epfl/rigel/math/Angle.java
@@ -2,37 +2,65 @@ package ch.epfl.rigel.math;
 
 public final class Angle {
     
-    private final static double TAU = 2 * Math.PI;
+    private static final double TAU = 2 * Math.PI;
+    
+    private static final double ARC_SECOND_PER_DEG = 1 / 3600.0;
+    
+    private static final double ARC_MINUTE_PER_DEG = 1 / 60.0;
+   
+    private static final double ARC_SECOND_PER_RAD = Math.toRadians(ARC_SECOND_PER_DEG);    
+    
+    private static final double ARC_HOUR_PER_RAD = TAU / 24;
+    
+    private static final double RAD_PER_ARC_HOUR = 24/ TAU;
+    
     
     private Angle () {}
     
     // normalise l'angle rad en le réduisant à l'intervalle [0,τ[,
-    public static double normalizePositive(double rad) {
-         
-        double normalizeAngle = rad;
+    public static double normalizePositive(double rad) {   
         
+        double multipleTAU = Math.floor(rad / TAU);       
         
+        rad -= TAU  * multipleTAU;       
         
-        double multipleTau = rad % TAU;
-        
-        System.out.println(multipleTau);
-        
-        normalizeAngle -= multipleTau * TAU;
-        
-        return normalizeAngle;
-    }
-    
-    
-    private double reduceAngle(double amount, double angle) {
-       // if()
-            
-            
-        return angle;
+        return rad;
+      
     }
     
     public static double ofArcsec(double sec) {
-        return 0;
+        return sec*ARC_SECOND_PER_RAD;
     }
+    
+    public static double ofDMS(int deg, int min, double sec) {
+        if( (deg < 0 && deg >= 60) || (min < 0 && min >= 60)) {
+            throw new IllegalArgumentException();
+        }
+        
+        double angle_deg = deg + min* ARC_MINUTE_PER_DEG + sec * ARC_SECOND_PER_DEG;
+        
+        return Math.toRadians(angle_deg);
+    }
+    
+    public static double ofDeg(double deg) {
+        return Math.toRadians(deg);
+    }
+    
+    public static double toDeg(double rad) {
+        return Math.toDegrees(rad);
+    }
+    
+    public static double ofHr(double hr) {
+        return hr* RAD_PER_ARC_HOUR;
+    } 
+    
+    
+    public static double toHr(double rad) {
+      return rad * ARC_HOUR_PER_RAD;
+    }
+    
+    
+  
     
     
   
