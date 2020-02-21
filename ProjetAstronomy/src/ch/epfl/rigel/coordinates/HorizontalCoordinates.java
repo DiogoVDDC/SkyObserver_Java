63200a4929100673265ff43500a564a846db88e8
Théo <theo.houle@epfl.ch>
2020-02-21 15:58:49 +0100

    HorizontalCoord

---
+++ ProjetAstronomy/src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
@@ -0,0 +1,34 @@
+package ch.epfl.rigel.coordinates;
+
+import ch.epfl.rigel.Preconditions;
+import ch.epfl.rigel.math.Angle;
+import ch.epfl.rigel.math.ClosedInterval;
+import ch.epfl.rigel.math.RightOpenInterval;
+
+public class HorizontalCoordinates extends SphericalCoordinates{
+
+    private static final RightOpenInterval azInterval = RightOpenInterval.of(0, 360);
+    private static final ClosedInterval altInterval = ClosedInterval.symmetric(180);
+    
+    private HorizontalCoordinates(double alt, double az) {
+        super(alt, az);
+    }
+    
+    HorizontalCoordinates of(double alt, double az) {
+        return new HorizontalCoordinates(Preconditions.checkInInterval(altInterval, alt), Preconditions.checkInInterval(azInterval, az));
+    }
+    
+    HorizontalCoordinates ofDeg(double azDeg, double altDeg) {
+        return new HorizontalCoordinates(Angle.ofDeg(Preconditions.checkInInterval(altInterval, altDeg)), Angle.ofDeg(Preconditions.checkInInterval(azInterval, azDeg)));
+    }
+    
+    public double az() {
+        return lon();
+    }
+    
+    public double azDeg() {
+        return lonDeg();
+    }
+    
+    public String azOctantName()
+}
