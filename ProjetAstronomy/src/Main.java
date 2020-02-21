1d8bb36b356ad0e0fdf774298a72f5e728fe7c22
diogo <diogo@128.179.156.56>
2020-02-21 13:41:08 +0100

    test completed

--- ProjetAstronomy/src/Main.java
+++ ProjetAstronomy/src/Main.java
@@ -1,9 +1,15 @@
+import ch.epfl.rigel.math.Interval;
 import ch.epfl.rigel.math.Polynomial;
+import ch.epfl.rigel.math.RightOpenInterval;
 
 public class Main {
     public static void main(String[] args) {
         Polynomial a = Polynomial.of(1, 2, 2);
         System.out.println(a.at(2));
         System.out.println(a);
+        
+        
+        Interval interval = RightOpenInterval.of(-5, -2);
+        System.out.println(interval);
     }
 }
