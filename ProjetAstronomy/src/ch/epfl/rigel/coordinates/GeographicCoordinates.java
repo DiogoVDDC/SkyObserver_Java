a25b712f216da03e983d632f9e1171cb83b7e411
Théo <theo.houle@epfl.ch>
2020-02-23 18:11:14 +0100

    GeographicCoordinatesTest & commentary & minor changes

--- ProjetAstronomy/src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
+++ ProjetAstronomy/src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
@@ -2,8 +2,6 @@ package ch.epfl.rigel.coordinates;
 
 import java.util.Locale;
 
-import org.junit.jupiter.api.Order;
-
 import ch.epfl.rigel.Preconditions;
 import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.ClosedInterval;
@@ -11,9 +9,9 @@ import ch.epfl.rigel.math.RightOpenInterval;
 
 public final class GeographicCoordinates extends SphericalCoordinates{
 
-    // longitude interval in terms of radians, 180 to -180 degrees
+    // longitude interval in terms of radiant, 180 to -180 degrees //degrees or radiant??
     private static final RightOpenInterval lonInterval = RightOpenInterval.symmetric(360);
-    // latitude interval in terms of radians, 90 to -90 degrees
+    // latitude interval in terms of radiant, 90 to -90 degrees  //degrees or radiant??
     private static final ClosedInterval latInterval = ClosedInterval.symmetric(180);
   
     private GeographicCoordinates(double lat, double lon) {
@@ -26,17 +24,23 @@ public final class GeographicCoordinates extends SphericalCoordinates{
      * @param: latitude in degrees 
      * @return: returns a new geographic coordinates object
      */
-     public static GeographicCoordinates ofDeg(double lonDeg, double latDeg) {
+     public static GeographicCoordinates ofDeg(double lonDeg, double latDeg) { //Maybe use the isValidLonDeg et isValidLatDeg instead of preconditions?
         return new GeographicCoordinates( Angle.ofDeg(Preconditions.checkInInterval(latInterval, latDeg)),
                 Angle.ofDeg(Preconditions.checkInInterval(lonInterval, lonDeg)));
     }
 
+     // SHOULD THEY NOT RATHER RETURN FALSE INSTEAD OF AN EXCPETION??
     /**     
      * @param: longitude in degrees 
      * @return: returns true is the longitude is in the correct interval
      */
     public static boolean isValidLonDeg(double lonDeg) {
-        Preconditions.checkInInterval(lonInterval, lonDeg);
+        try {
+            Preconditions.checkInInterval(lonInterval, lonDeg);
+        } 
+        catch(IllegalArgumentException e) {
+            return false;
+        }
         return true;
     }
 
@@ -45,7 +49,12 @@ public final class GeographicCoordinates extends SphericalCoordinates{
      * @return: returns true is the latitude is in the correct interval
      */
     public static boolean isValidLatDeg(double latDeg) {
-        Preconditions.checkInInterval(lonInterval, latDeg);
+        try {
+            Preconditions.checkInInterval(latInterval, latDeg);
+        }
+        catch(IllegalArgumentException e) {
+            return false;
+        }
         return true;
     }
 
@@ -77,9 +86,8 @@ public final class GeographicCoordinates extends SphericalCoordinates{
         return super.latDeg();
     }
 
-    // HOW DO I ADD THE DEGREE SYMBOL
     @Override
     public String toString(){
-        return String.format(Locale.ROOT,"(lon=%.4f, lat=%.4f)", lonDeg(), latDeg());
+        return String.format(Locale.ROOT,"(lon=%.4f°, lat=%.4f°)", lonDeg(), latDeg());
     }
 }
--- ProjetAstronomy/test/ch/epfl/coordinates/GeographicCoordinatesTest.java
+++ ProjetAstronomy/test/ch/epfl/coordinates/GeographicCoordinatesTest.java
@@ -1,5 +1,88 @@
 package ch.epfl.coordinates;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import ch.epfl.rigel.math.Angle;
+
 public class GeographicCoordinatesTest {
 
+    @Test
+    void withLegalArgumentsTests() {
+        GeographicCoordinates coord = GeographicCoordinates.ofDeg(150, 50);
+        assertEquals(150, coord.lonDeg());
+        assertEquals(50, coord.latDeg());
+        
+        assertEquals(Angle.ofDeg(150), coord.lon());
+        assertEquals(Angle.ofDeg(50), coord.lat());
+        
+        
+        GeographicCoordinates coord_2 = GeographicCoordinates.ofDeg(179, 50);
+        assertEquals(179, coord_2.lonDeg());
+        assertEquals(50, coord_2.latDeg());
+        
+        GeographicCoordinates coord_3 = GeographicCoordinates.ofDeg(150, 90);
+        assertEquals(150, coord_3.lonDeg());
+        assertEquals(90, coord_3.latDeg());
+        
+        GeographicCoordinates coord_4 = GeographicCoordinates.ofDeg(150, -90);
+        assertEquals(150, coord_4.lonDeg());
+        assertEquals(-90, coord_4.latDeg());
+    }
+        
+    @Test
+    void withIllegalArgumentsTests() {
+        assertThrows(IllegalArgumentException.class, () ->{
+            GeographicCoordinates.ofDeg(180, 50); 
+        });
+        assertThrows(IllegalArgumentException.class, () ->{
+            GeographicCoordinates.ofDeg(150, 91);
+        });
+        
+        assertThrows(IllegalArgumentException.class, () ->{
+            GeographicCoordinates.ofDeg(400, 120); 
+        });
+    }
+    
+    @Test
+    void isValidLonDegTests() {
+        assertTrue(GeographicCoordinates.isValidLonDeg(-160));
+        assertTrue(GeographicCoordinates.isValidLonDeg(-34.5478));
+        assertTrue(GeographicCoordinates.isValidLonDeg(-10));
+        assertTrue(GeographicCoordinates.isValidLonDeg(160));
+        assertTrue(GeographicCoordinates.isValidLonDeg(113.5739));
+        assertTrue(GeographicCoordinates.isValidLonDeg(0));
+        assertTrue(GeographicCoordinates.isValidLonDeg(179));
+        assertTrue(GeographicCoordinates.isValidLonDeg(-180));
+        assertTrue(GeographicCoordinates.isValidLonDeg(179.9999));
+        
+        assertFalse(GeographicCoordinates.isValidLonDeg(-190));
+        assertFalse(GeographicCoordinates.isValidLonDeg(-180.001));
+        assertFalse(GeographicCoordinates.isValidLonDeg(-200));
+        assertFalse(GeographicCoordinates.isValidLonDeg(180));
+        assertFalse(GeographicCoordinates.isValidLonDeg(192.3628));
+        assertFalse(GeographicCoordinates.isValidLonDeg(200.1234));
+    }
+
+    @Test
+    void isValidLatDegTests() {
+        assertTrue(GeographicCoordinates.isValidLatDeg(-30));
+        assertTrue(GeographicCoordinates.isValidLatDeg(30));
+        assertTrue(GeographicCoordinates.isValidLatDeg(-21.234324));
+        assertTrue(GeographicCoordinates.isValidLatDeg(60.43627));
+        assertTrue(GeographicCoordinates.isValidLatDeg(-90));
+        assertTrue(GeographicCoordinates.isValidLatDeg(90));
+        assertTrue(GeographicCoordinates.isValidLatDeg(0));
+        
+        assertFalse(GeographicCoordinates.isValidLatDeg(90.0001));
+        assertFalse(GeographicCoordinates.isValidLatDeg(100));
+        assertFalse(GeographicCoordinates.isValidLatDeg(-100));
+        assertFalse(GeographicCoordinates.isValidLatDeg(-90.0001));
+    }
 }
