de3053862f66490a28a2bb69b6ac278da8a81cd2
Théo <theo.houle@epfl.ch>
2020-03-06 15:40:04 +0100

    pigeon

--- ProjetAstronomy/src/Main.java
+++ ProjetAstronomy/src/Main.java
@@ -1,21 +1,10 @@
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-
-import ch.epfl.rigel.astronomy.Epoch;
-import ch.epfl.rigel.astronomy.SiderealTime;
+import ch.epfl.rigel.astronomy.Moon;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
-import ch.epfl.rigel.coordinates.EquatorialToHorizontalConversion;
-import ch.epfl.rigel.coordinates.GeographicCoordinates;
-import ch.epfl.rigel.math.Angle;
-
-
 
 public class Main {
     public static void main(String[] args) {
+        Moon lu = new Moon(EquatorialCoordinates.of(1, 1), 10, 10, 0.3752f);
+        System.out.println(lu);
     }
 }
        
