9b0a3894e4e6a4102d18dd25ee3509f74e765dcd
Théo <theo.houle@epfl.ch>
2020-03-09 14:55:42 +0100

    Asterism

--- ProjetAstronomy/src/Main.java
+++ ProjetAstronomy/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import ch.epfl.rigel.astronomy.Asterism;
+import ch.epfl.rigel.astronomy.Star;
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+
 public class Main {
     public static void main(String[] args) {
+        Star star = new Star(2, "OUI", EquatorialCoordinates.of(2, 1), 2f, .5f);
+        List<Star> lis = new ArrayList<Star>(Collections.nCopies(4, star));
+        Asterism ast = new Asterism(lis);
+        System.out.println(ast.stars());
+        Star star2 = new Star(2, "NON", EquatorialCoordinates.of(2, 1), 2f, .5f);
+        List<Star> lis2 = ast.stars();
+        
+        System.out.println(lis2.get(1));
     }
 }
        
--- ProjetAstronomy/src/ch/epfl/rigel/astronomy/Asterism.java
+++ ProjetAstronomy/src/ch/epfl/rigel/astronomy/Asterism.java
@@ -5,14 +5,31 @@ import java.util.Objects;
 
 import ch.epfl.rigel.Preconditions;
 
+/**
+ * Classe representating an asterism.
+ * @author Theo Houle (312432)
+ *
+ */
 public final class Asterism {
     
+    // list of stars in the asterism
     private final List<Star> asterism;
     
+    /**
+     * Constructor for an asterism.
+     * @param stars: the list of star contained in the asterism.
+     */
     public Asterism(List<Star> stars){
         Objects.requireNonNull(stars);
         Preconditions.checkArgument(!stars.isEmpty());
-        
-        
+        asterism = List.copyOf(stars);
+    }
+    
+    /**
+     * Getter for the stars of the asterism
+     * @return: list of stars.
+     */
+    public List<Star> stars(){
+        return asterism;
     }
 }
