47bb7c42613cbea365d8c537a6e1cb09e4be176c
Théo Samuel Houle <theo.houle@epfl.ch>
2020-03-16 14:48:49 +0100

    corrigé PlanetModel

--- ProjetAstronomy/src/ch/epfl/rigel/astronomy/PlanetModel.java
+++ ProjetAstronomy/src/ch/epfl/rigel/astronomy/PlanetModel.java
@@ -10,7 +10,7 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
 
 	MERCURY("Mercure", 0.24085, 75.5671, 77.612, 0.205627,
 	        0.387098, 7.0051, 48.449, 6.74, -0.42),
-	VENUS("VÃ©nus", 0.615207, 272.30044, 131.54, 0.006812,
+	VENUS("Vénus", 0.615207, 272.30044, 131.54, 0.006812,
 	      0.723329, 3.3947, 76.769, 16.92, -4.40),
 	EARTH("Terre", 0.999996, 99.556772, 103.2055, 0.016671,
 	      0.999985, 0, 0, 0, 0),
@@ -41,16 +41,16 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
 	private final double orbitTiltAtEcliptic;
 	// Longitude of the orbital node.
 	private final double lonOrbitalNode;
-	//Angular size of the planet.
-	private final double angularSize;
-	// Magnitude of the planet.
-	private final double magnitude;
+	//Angular size of the planet at a distance of 1 UA.
+	private final double angularSize1UA;
+	// Magnitude of the planet at a distance of 1 UA.
+	private final double magnitude1UA;
 	// Immutable list containing all the object of this enum.
 	public static List<PlanetModel> ALL = List.of(PlanetModel.MERCURY, PlanetModel.VENUS, PlanetModel.EARTH,
 				PlanetModel.MARS, PlanetModel.JUPITER, PlanetModel.SATURN, PlanetModel.URANUS, PlanetModel.NEPTUNE);
 	
 	private PlanetModel(String frenchName, double revoPeriod, double lonAtJ2010, double lonAtPerigee, double orbitEccentricity,
-						double semiMajorAxis, double orbitTiltAtEcliptic, double lonOrbitalNode, double angularSize, double magnitude) {
+						double semiMajorAxis, double orbitTiltAtEcliptic, double lonOrbitalNode, double angularSize1UA, double magnitude1UA) {
 		this.frenchName = frenchName;
 		this.revoPeriod = revoPeriod;
 		this.lonAtJ2010 = Angle.ofDeg(lonAtJ2010);
@@ -59,8 +59,8 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
 		this.semiMajorAxis = semiMajorAxis;
 		this.orbitTiltAtEcliptic = Angle.ofDeg(orbitTiltAtEcliptic);
 		this.lonOrbitalNode = Angle.toDeg(lonOrbitalNode);
-		this.angularSize = angularSize;
-		this.magnitude = magnitude;
+		this.angularSize1UA = Angle.ofArcsec(angularSize1UA);
+		this.magnitude1UA = magnitude1UA;
 	}
 	
 	private double trueAnomaly(double daysSinceJ2010, PlanetModel planet) {
@@ -88,19 +88,20 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
 		double rProj = r * Math.cos(helioCenEclipticLat);
 		//Longitude projected on ecliptic plan.
 		double lonInOwnOrbitProj = Math.atan2((Math.sin(lonInOwnOrbit - lonOrbitalNode) * Math.cos(orbitTiltAtEcliptic)),
-									Math.cos(lonInOwnOrbit - lonOrbitalNode)) + lonOrbitalNode;
+									Math.cos(lonInOwnOrbit - lonOrbitalNode));
 		
 		double eclipticLon;
 		switch(frenchName) {
-			case "VÃ©nus": case "Mercure": case "Terre":
+			case "Mercure":
+			case "Vénus":
 				//Calculus of longitude for inner planets.
 				eclipticLon = Math.PI + lonInOwnOrbitEarth + Math.atan2(rProj * Math.sin(lonInOwnOrbitEarth - lonInOwnOrbitProj),
 									rEarth - rProj * Math.cos(lonInOwnOrbitEarth - lonInOwnOrbitProj));
 				break;
 			default:
 				//Calculus of longitude for outer planets.
-				eclipticLon = Angle.normalizePositive(lonInOwnOrbitProj + Math.atan2(rEarth * Math.sin(lonInOwnOrbitProj - lonInOwnOrbitEarth),
-								rProj - rEarth * Math.cos(lonInOwnOrbitProj - lonInOwnOrbitEarth)));
+				eclipticLon = lonInOwnOrbitProj + Math.atan2(rEarth * Math.sin(lonInOwnOrbitProj - lonInOwnOrbitEarth),
+								rProj - rEarth * Math.cos(lonInOwnOrbitProj - lonInOwnOrbitEarth));
 				break;
 		}
 		
@@ -110,6 +111,17 @@ public enum PlanetModel implements CelestialObjectModel<Planet> {
 				
 		//Ecliptic coordinates of the planet
 		EclipticCoordinates eclipticPos = EclipticCoordinates.of(eclipticLon, eclipticLat);
+		
+		//Distance from earth squared
+		double distFromEarth = Math.pow(rEarth, 2) + Math.pow(r, 2) - 2 * rEarth * r *Math.cos(lonInOwnOrbit - lonInOwnOrbitEarth) * Math.cos(helioCenEclipticLat); 
+		
+		//Angular size
+		double angularSize = angularSize1UA / Math.sqrt(distFromEarth);
+		
+		//Phase of the planet.
+		double F = (1 + Math.cos(eclipticLon - lonInOwnOrbit)) / 2;
+		//Magnitude of the planet
+		double magnitude = magnitude1UA + 5 * Math.log10((r * Math.sqrt(distFromEarth)) / Math.sqrt(F)); 
 		
 		return new Planet(frenchName, eclipticToEquatorialConversion.apply(eclipticPos), (float)angularSize, (float)magnitude);
 	}
