75ed441f998cd4497a917c09090e03f0fb5d31a3
Théo Samuel Houle <theo.houle@epfl.ch>
2020-04-24 15:34:29 +0200

    TimeAnimator & few comments in DateTimeBean

--- ProjetAstronomy/src/ch/epfl/rigel/gui/DateTimeBean.java
+++ ProjetAstronomy/src/ch/epfl/rigel/gui/DateTimeBean.java
@@ -7,18 +7,32 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
+/**
+ * Class representing an observable ZonedDateTime.
+ * @author Theo Houle (312432)
+ *
+ */
 public final class DateTimeBean {
 
+	//The date property.
 	private final ObjectProperty<LocalDate> date;
+	//The time property.
 	private final ObjectProperty<LocalTime> time;
+	//The zone property.
 	private final ObjectProperty<ZoneId> zone;
 	
-	public DateTimeBean(ObjectProperty<LocalDate> date, ObjectProperty<LocalTime> time,
-			ObjectProperty<ZoneId> zone) {
-		this.date = date;
-		this.time = time;
-		this.zone = zone;
+	/**
+	 * DateTimeBean constructor.
+	 * @param date: the initial local date.
+	 * @param time: the initial local time
+	 * @param zone: the initial zone.
+	 */
+	public DateTimeBean(LocalDate date, LocalTime time, ZoneId zone) {
+		this.date = new SimpleObjectProperty<>(date);
+		this.time = new SimpleObjectProperty<>(time);
+		this.zone = new SimpleObjectProperty<>(zone);
 	}
 	
 	/**
@@ -30,14 +44,14 @@ public final class DateTimeBean {
 	}
 	/**
 	 * Getter for the date propertie's date.
-	 * @return
+	 * @return: the date property.
 	 */
 	public LocalDate getDate() {
 		return date.getValue();
 	}
 	/**
 	 * Setter for the date property's date.
-	 * @param newDate
+	 * @param newDate: the new date.
 	 */
 	public void setDate(LocalDate newDate) {
 		date.set(newDate);
@@ -45,7 +59,7 @@ public final class DateTimeBean {
 	
 	/**
 	 * Getter for the time porperty.
-	 * @return
+	 * @return: the time property.
 	 */
 	public ObjectProperty<LocalTime> timeProperty(){
 		return time;
@@ -53,14 +67,14 @@ public final class DateTimeBean {
 	
 	/**
 	 * Getter for the time property's time.
-	 * @return
+	 * @return : the time of the time property.
 	 */
 	public LocalTime getTime() {
 		return time.getValue();
 	}
 	/**
 	 * Setter for the time property's time.
-	 * @param newTime
+	 * @param newTime:t the new local time.
 	 */
 	public void setTime(LocalTime newTime) {
 		time.set(newTime);
@@ -68,34 +82,43 @@ public final class DateTimeBean {
 	
 	/**
 	 * Getter for the zone property.
-	 * @return
+	 * @return: the zone property.
 	 */
 	public ObjectProperty<ZoneId> zoneProperty(){
 		return zone;
 	}
 	/**
 	 * Getter for the zone property's zone.
-	 * @return
+	 * @return: the zone of the zone property.
 	 */
 	public ZoneId getZone() {
 		return zone.getValue();
 	}
 	/**
 	 * Setter for the time property's time.
-	 * @param newZone
+	 * @param newZone: the new zoneId.
 	 */
 	public void setZone(ZoneId newZone) {
 		zone.setValue(newZone);
 	}
 	
+	/**
+	 * Getter for the triplet date, time and zone as a ZonedDateTime.
+	 * @return: the ZonedDateTime created using the time, date
+	 * and zone properties.
+	 */
 	public ZonedDateTime getZonedDateTime() {
 		return ZonedDateTime.of(LocalDateTime.of(date.getValue(), time.getValue()),
 				zone.getValue());
 	}
 	
+	/**
+	 * Setter for the whole triplet date, time and zone using ZonedDateTime.
+	 * @param newZonedDateTime: the new ZonedDateTime.
+	 */
 	public void setZonedDateTime(ZonedDateTime newZonedDateTime) {
-		setDate(newZonedDateTime.toLocalDate());
-		setTime(newZonedDateTime.toLocalTime());
-		setZone(newZonedDateTime.getZone());
+		date.setValue(newZonedDateTime.toLocalDate());
+		time.setValue(newZonedDateTime.toLocalTime());
+		zone.setValue(newZonedDateTime.getZone());
 	}
 }
---
+++ ProjetAstronomy/src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -0,0 +1,110 @@
+package ch.epfl.rigel.gui;
+
+import java.time.ZonedDateTime;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+/**
+ * Class representing a time animator which periodically updates a given time 
+ * based on a time accelerator.
+ * @author Theo Houle (312432)
+ *
+ */
+public final class TimeAnimator extends AnimationTimer{
+
+	//The observable date.
+	private final DateTimeBean dateTime;
+	//Observable boolean to know wether the animation is running or not.
+	private final SimpleBooleanProperty running;
+	//Observable accelerator allowing to update the time.
+	private final ObjectProperty<TimeAccelerator> accelerator;
+	//Pointer of the time since the beggining of the clock gievn by the last call of handle method.
+	private long elapsedTime;
+	//Boolean allowing to know if the start method has just been called.
+	private boolean justStarted;	
+	
+	/**
+	 * TimeAnimator constructor.
+	 * @param dateTime: initial time.
+	 */
+	public TimeAnimator(DateTimeBean dateTime) {
+		this.dateTime = dateTime;
+		accelerator = new SimpleObjectProperty<TimeAccelerator>(null);
+		running = new SimpleBooleanProperty();
+		elapsedTime = 0;
+		justStarted = false;
+	}
+	
+	@Override
+	public void start() {
+		super.start();
+		justStarted = true;
+		running.setValue(true);
+	}
+	
+	@Override
+	public void stop() {
+		super.stop();
+		running.setValue(false);
+	}
+
+	@Override
+	public void handle(long now) {
+		//If handle is being called for the first time, stores the initial time value.
+		if (justStarted) {
+			elapsedTime =  now;
+			justStarted = false;
+		} else {
+			//Compute elapsed time since last call.
+			long deltaTime = now - elapsedTime;
+			//Adjut the date time according to the delta of time elapsed.
+			ZonedDateTime newObservedTime = accelerator.getValue().adjust(dateTime.getZonedDateTime(), deltaTime);
+			dateTime.setZonedDateTime(newObservedTime);
+			//Update the elapsed time.
+			elapsedTime = now;
+		}
+	}
+	
+	/**
+	 * Getter for the running property.
+	 * @return: the running property.
+	 */
+	public ReadOnlyBooleanProperty runningProperty() {
+		return (ReadOnlyBooleanProperty) ReadOnlyBooleanProperty.booleanExpression(running);
+	}	
+	/**
+	 * Getter for the running property's boolean value.
+	 * @return: boolean value of the running property.
+	 */
+	public boolean isRunning() {
+		return running.getValue();
+	}
+	
+	/**
+	 * Getter for the accelerator propety.
+	 * @return: the accelertor property.
+	 */
+	public ObjectProperty<TimeAccelerator> acceleratorProperty(){
+		return accelerator;
+	}
+	/**
+	 * Getter for the accelerator property's time accelerator.
+	 * @return
+	 */
+	public TimeAccelerator getAccelerator() {
+		return accelerator.getValue();
+	}
+	
+	/**
+	 * Setter for the accelerator property's time accelerartor.
+	 * @param newAccelerator: the new time accelerator.
+	 */
+	public void setAccelerator(TimeAccelerator newAccelerator) {
+		accelerator.setValue(newAccelerator);
+	}
+	
+}
